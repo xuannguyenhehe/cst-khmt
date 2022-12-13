597fa9c22c038a800ec8f224f30c150050ba3991
VTS-GPMN-AI <vts.gpmn.ai@gmail.com>
2022-12-13 07:06:45 +0700

    config params with leiden

--- assignment_3/leiden/leiden.py
+++ assignment_3/leiden/leiden.py
@@ -1,19 +1,12 @@
 import time
 import pandas as pd
-import igraph as ig
-t_0 = time.time()
-df_topsong = pd.read_csv("/content/edge.csv")
-g = ig.Graph.TupleList(df_topsong[["st_id_x", "st_id_y" ]].itertuples(index=False), weights = False, directed = False)
-print("TOTAL TIME: ", time.time() - t_0)
-
 import leidenalg
-t_0 = time.time()
-# part = g.community_leiden()
-part = leidenalg.find_partition(g, leidenalg.ModularityVertexPartition)
-max_idx = 0
-print(part.modularity)
-print("TOTAL TIME: ", time.time() - t_0)
-keys = ["id", "cluster"]
-result = [dict(zip(keys, [g.vs["name"][i], part.membership[i] + 1 + max_idx])) for i in range(len(part.membership))]
-df = pd.DataFrame(result)
-print(df)
+
+def leiden_communities(G):
+    part = leidenalg.find_partition(G, leidenalg.ModularityVertexPartition)
+    max_idx = 0
+    print(part.modularity)
+    keys = ["id", "cluster"]
+    result = [dict(zip(keys, [G.vs["name"][i], part.membership[i] + 1 + max_idx])) for i in range(len(part.membership))]
+    df = pd.DataFrame(result)
+    return df
--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import networkx as nx
 from louvain.louvain import louvain_communities
+from leiden.leiden import leiden_communities
+import igraph as ig
 
 def preprocess(path_edges: str, column_name_first: str, column_name_second: str, path_nodes: str):
     df_edge = pd.read_csv(path_edges)
@@ -15,12 +17,23 @@ def preprocess(path_edges: str, column_name_first: str, column_name_second: str,
     return df_edge
 
 if __name__ == "__main__":
-    path_edges = 'assignment_3/dataset/artist_spotify_edge.csv'
-    path_nodes = 'assignment_3/dataset/artist_spotify_node.csv'
+    import sys
+    if len(sys.argv) > 1:  # At least 1 command line parameter
+        type_algorithm = str(sys.argv[1])
+
+    path_edges = 'dataset/artist_spotify_edge.csv'
+    path_nodes = 'dataset/artist_spotify_node.csv'
 
     df_edge = preprocess(path_edges, 'st_id_x', 'st_id_y', path_nodes)    
-    G = nx.from_pandas_edgelist(df_edge, 'st_id_x', 'st_id_y')
-    list_clusters = louvain_communities(G, seed=21)
-    for i in list_clusters:
-        print(len(i), sum([len(j) for j in i]), i) 
-        
+
+    if type_algorithm == "louvain":
+        G = nx.from_pandas_edgelist(df_edge, 'st_id_x', 'st_id_y')
+        list_clusters = louvain_communities(G, seed=21)
+        for i in list_clusters:
+            print(len(i), sum([len(j) for j in i]), i) 
+    elif type_algorithm == "leiden":
+        G = ig.Graph.TupleList(df_edge.itertuples(index=False), weights = False, directed = False)
+        list_clusters = leiden_communities(G)
+        print(list_clusters)
+    else:
+        raise ValueError("Could not find type of algorithm you need")
